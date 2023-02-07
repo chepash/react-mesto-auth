6ff8ac53ab39b6f3da800fb288f5cc9df9a39c9b
Pavel Cherdantsev <ch3pash@yandex.ru>
2023-02-08 00:06:30 +0400

    hotfix: HamburgerButton <label> fix

--- src/components/HamburgerButton.js
+++ src/components/HamburgerButton.js
@@ -2,7 +2,7 @@ function HamburgerButton() {
   return (
     <>
       <input id="toggle" type="checkbox" className="page__toggle" />
-      <label for="toggle" className="button button_type_hamburger">
+      <label htmlFor="toggle" className="button button_type_hamburger">
         <div className="top-bun button__part"></div>
         <div className="meat button__part"></div>
         <div className="bottom-bun button__part"></div>
